026f0b3b261873095f3dc6b50eb0715d0d877d81
sumin <164634182+sumin-coder@users.noreply.github.com>
2026-03-04 20:56:32 +0900

    더보기 창

---
+++ Practice/drawer/first_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("1st Page")),
+      drawer: Drawer(
+        backgroundColor: Colors.deepPurple[100],
+        child: Column(
+          children: [
+            // common to olace a drwer header here
+            DrawerHeader(
+              child: Icon(Icons.favorite, size: 48),
+            ),
+
+            // home page list title
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("H O M E"),
+              onTap: () {
+                // go to home page
+                Navigator.pushNamed(context, '/homepage');
+              },
+            ),
+
+            // setting page list title
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("S E T T I N G S"),
+              onTap: () {
+                // go to settings page
+                Navigator.pushNamed(
+                  context,
+                  '/settingspage',
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
